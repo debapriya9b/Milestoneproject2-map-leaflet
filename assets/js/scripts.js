fde95fa7958c7e24860f96415cd6b4ac2dbcced6
Ubuntu <ubuntu@ip-172-31-39-215.ec2.internal>
2019-07-01 22:20:27 +0000

    Js for modal to open when page loaded added

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+/* global L */
+
+//-------------------------------------------------Modal
+
+//Modal will open with page load
+
+$(document).ready(function() {
+    $("#myModal").modal('show');
+});
