c9083650d651f81d10f1a7406ac90161ede6b76d
Ubuntu <ubuntu@ip-172-31-39-215.ec2.internal>
2019-07-01 22:21:53 +0000

    Map tile,popup added in JS file

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,3 +7,19 @@
 $(document).ready(function() {
     $("#myModal").modal('show');
 });
+
+//------------------------------------------------Creating map/Tile Layer/Popup
+
+
+//Creating map options
+var mapOptions = {
+    center: [40.4168, -3.7038],
+    zoom: 2,
+    minZoom: 2,
+    maxZoom: 18,
+    maxBounds: [
+        [-75, -190],
+        [90, 190]
+    ],
+    maxBoundsViscosity: 0.5,
+}
